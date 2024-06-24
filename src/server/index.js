88a5f9675533eef2f7a1e3c03c1372b5f1f831a9
gigimoose23 <88978318+gigimoose23@users.noreply.github.com>
2024-06-24 06:33:14 +0000

    other

---
+++ src/server/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const noblox = require("noblox.js");
+const app = express();
+
+
+
+
+app.listen(8080, () => {
+    console.log("✅ Running InsertService Remade Server")
+});
